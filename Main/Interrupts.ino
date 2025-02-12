58846b8e8538158fb94c7f9df8aaeca1117eae14
Thomas Farstrike <thomasverstreken@protonmail.com>
2025-02-12 21:11:29 +0100

    Add long-press handler for General Purpose button

--- Main/Interrupts.ino
+++ Main/Interrupts.ino
@@ -9,21 +9,37 @@ long minTimeBetweenTilts = 2000;
 long lastTilted = NOT_SPECIFIED;
 bool tiltMessageShown = false;
 
+volatile unsigned long userButtonPushMicros  = 0;
+volatile unsigned long userButtonReleaseMicros  = 0;
+
 void IRAM_ATTR interruptHandler() {
   portENTER_CRITICAL_ISR(&mux);
   interruptTriggered = true;
   portEXIT_CRITICAL_ISR(&mux);
 }
 
+void IRAM_ATTR interruptHandlerUserButton() {
+  if (digitalRead(GPIO_NUM_39) == HIGH) {
+        userButtonReleaseMicros = micros();
+    } else {
+        userButtonPushMicros = micros();
+    }
+}
+
+
 void setup_interrupts() {
-  Serial.println("configuring pin 32 as interrupt...");
+  Serial.println("configuring pin GPIO_NUM_32 as interrupt...");
   pinMode(GPIO_NUM_32, INPUT_PULLDOWN);
   attachInterrupt(GPIO_NUM_32, interruptHandler, HIGH);
+
+  Serial.println("configuring pin GPIO_NUM_39 as interrupt...");
+  pinMode(GPIO_NUM_39, INPUT_PULLUP);
+  attachInterrupt(GPIO_NUM_39, interruptHandlerUserButton, CHANGE);
 }
 
 void loop_interrupts() {
   if (interruptTriggered && (millis() - lastInterrupt > minTimeBetweenInterrupts)) {
-    Serial.println("interrupt triggered! handling it...");
+    Serial.println("tilt sensor interrupt triggered! handling it...");
     lastInterrupt = millis();
     portENTER_CRITICAL(&mux);
     interruptTriggered = false;
@@ -45,4 +61,13 @@ void loop_interrupts() {
       nextRefreshBalanceAndPayments();
     }
   }
+
+  unsigned long pushDuration = userButtonReleaseMicros - userButtonPushMicros;
+  if (pushDuration > 3*1000*1000 && pushDuration < 30*1000*1000) {
+    displayFit("User button was pushed for more than 3s, starting Access Point configuration mode!", 0, 0, displayWidth(), displayHeight(), MAX_FONT);
+    piggyMode = PIGGYMODE_STARTING_AP;
+    userButtonPushMicros = 0;
+    userButtonReleaseMicros = 0;
+  }
+
 }
