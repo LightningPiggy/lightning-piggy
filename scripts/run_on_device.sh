2c61d7235f74ef019f623e300f12bb010c5b119c
Thomas Farstrike <thomasverstreken@protonmail.com>
2025-03-17 14:14:06 +0100

    run_on_device.sh: make config_dir optional

--- scripts/run_on_device.sh
+++ scripts/run_on_device.sh
@@ -9,12 +9,15 @@ PARTITIONSNAME=$(find "$SOFTWARE_DIR" -iname "*ino.partitions.bin" )
 
 BOOTAPP=~/.arduino15/packages/esp32/hardware/esp32/*/tools/partitions/boot_app0.bin
 
-if [ -d config_partition/ ]; then
-	echo "Folder config_partition/ found, creating LittleFS configuration filesystem..."
-	~/sources/mklittlefs/mklittlefs -c config_partition/ -s 0x70000 config_partition.bin
+configdir="$1"
+configarg=""
+if [ ! -z "$configdir" -a -d "$configdir"/ ]; then
+	echo "Folder $configdir found, creating LittleFS configuration filesystem..."
+	~/sources/mklittlefs/mklittlefs -c "$configdir"/ -s 0x70000 config_partition.bin
+        configarg="0x390000 config_partition.bin"
 	[ $? -ne 0 ] && echo "ERROR: could not create LittleFS configuration filesystem" && exit 1
 fi
 
-esptool.py --baud 921600 --after hard_reset write_flash 0x1000 "$BOOTLOADERNAME" 0x8000 "$PARTITIONSNAME" 0xe000 $BOOTAPP 0x10000 $APPNAME 0x390000 config_partition.bin
+esptool.py --baud 921600 --after hard_reset write_flash 0x1000 "$BOOTLOADERNAME" 0x8000 "$PARTITIONSNAME" 0xe000 $BOOTAPP 0x10000 $APPNAME $configarg
 
-picocom -b 115200 /dev/ttyACM0
+picocom -b 115200 /dev/ttyACM*
